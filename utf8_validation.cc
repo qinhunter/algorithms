f3df7d0a08e177fdf9f83bb93846a2a56d9d5a24
qinweite <qinweite@baidu.com>
2022-09-13 19:47:15 +0800

    utf8_validation.cc

--- utf8_validation.cc
+++ utf8_validation.cc
@@ -7,15 +7,16 @@ public:
             if ((data[i] & f1) == 0) {
                 continue;
             } else if ((data[i] & f2) == f2e) {
-                if (!helper(data[++i])) {
+                if (data.size() <= i + 1 || !helper(data[++i])) {
                     return false;
                 }
             } else if ((data[i] & f3) == f3e) {
-                if (!helper(data[++i]) || !helper(data[++i])) {
+                if (data.size() <= i + 2|| !helper(data[++i]) || !helper(data[++i])) {
                     return false;
                 }
             } else if ((data[i] & f4) == f4e) {
-                if (!helper(data[++i]) || !helper(data[++i]) || !helper(data[++i])) {
+                if (data.size() <= i + 3|| !helper(data[++i]) || !helper(data[++i]) || !helper(data[++i])) {
+                    printf("hunter test\n");
                     return false;
                 }
             } else {
@@ -29,7 +30,7 @@ public:
     int f2e = 3 << 6;
     int f3 = 15 << 4;
     int f3e = f2;
-    int f4 = 31 << 23;
+    int f4 = 31 << 3;
     int f4e = f3;    
     int fs = 3 << 6;
     bool helper(const int d) {
@@ -39,7 +40,7 @@ public:
 
 int main() {
     Solution sol;
-    std::vector<int> nums{235,140,4};
+    std::vector<int> nums{240,162,138,147};
     for (const auto num : nums) {
         binary_print(num);
     }
